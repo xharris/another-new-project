5c7084d30f9ca1f1247bb86f400b26e8e400e94a
Xavier Harris <xhh950@gmail.com>
2016-09-29 14:23:34 -0400

    the first commit

---
+++ src/js/main.js
@@ -0,0 +1,242 @@
+var IDE_NAME = "BlankE";
+var ZOOM_AMT = 1;
+var DEV_MODE = true; // use dev_data instead of data for saving
+
+require('electron-cookies');
+
+var nwFILE = require('fs');
+var nwPATH = require('path');
+var nwPROC = require('process');
+var nwCHILD = require('child_process');
+var nwOS = require('os');
+var nwNET = require('net');
+
+var nwMAC = require("getmac");
+//var nwUA = require("universal-analytics");
+
+var eIPC = require('electron').ipcRenderer;
+var eREMOTE = require('electron').remote;
+var eAPP = eREMOTE.require('electron').app;
+var eSHELL = eREMOTE.require('electron').shell;
+
+var editor, aceModeList;
+var re_file_ext = /(?:\.([^.]+))?$/;
+
+
+$(function(){
+    /* disable eval
+    window.eval = global.eval = function() {
+      throw new Error("Sorry, BlankE does not support window.eval() for security reasons.");
+    };
+    */
+
+    $("body").addClass(nwOS.type());
+
+    /* title bar buttons
+    $(".title-bar #btn-close").on("click", function() {
+        eIPC.send('close');
+    });
+    $(".title-bar #btn-maximize").on("click", function() {
+        eIPC.send('maximize');
+    });
+    $(".title-bar #btn-minimize").on("click", function() {
+        eIPC.send('minimize');
+    });
+    */
+
+    // set user id
+    nwMAC.getMac(function(err, address) {
+       if (!err && !DEV_MODE) {
+            var hash = address.hashCode();
+        //   analytics.userID = hash;
+        //   analytics.clientID = hash;
+            
+            console.log("userID: " + hash);
+       }
+    });
+
+    //analytics.event('UI', 'initialize', 'main_window', '');
+
+    // set events for window close
+    eIPC.on('window-close', function(event) {
+        // b_ide.saveData();
+        eIPC.send('confirm-window-close');
+    });
+
+    /*
+    var drop_mainwin = document.getElementById("body");
+	drop_mainwin.ondragover = () => {
+        // console.log(e.dataTransfer.files);
+		if ($(".filedrop-overlay").hasClass("inactive")) {
+			//$(".filedrop-overlay").removeClass("inactive");
+		}
+		return false;
+	};
+	drop_mainwin.ondragleave = drop_mainwin.ondragend = () => {
+		//$(".filedrop-overlay").addClass("inactive");
+		return false;
+	};
+	drop_mainwin.ondrop = (e) => {
+		e.preventDefault();
+
+		for (var f of e.dataTransfer.files) {
+			var in_path = f.path;
+
+            handleDropFile(in_path);
+
+		}
+		$(".filedrop-overlay").addClass("inactive");
+		return false;
+	};
+    */
+
+    var args = eREMOTE.getGlobal("shareVars").args;
+
+    if (args.length >= 3) {
+        var in_file = args[2];
+
+        handleDropFile(in_file);
+    }
+});
+
+function handleDropFile(in_path) {
+    nwFILE.lstat(in_path, function(err, stats) {
+        if (!err) {
+            if (stats.isDirectory()) {
+                var folder_name = nwPATH.basename(in_path);
+
+                b_project.addFolder(in_path)
+            }
+            else if (stats.isSymbolicLink()) {
+                b_ide.addToast({
+                    message: " symoblic! " + in_path,
+                    can_dismiss: false,
+                    timeout: 2000
+                });
+            }
+            else if (stats.isFile()) {
+                /*
+                b_ide.addToast({
+                    message: labels['file'] + " " + in_path,
+                    can_dismiss: false,
+                    timeout: 2000
+                });
+
+                b_project.reset();
+                b_project.setFolder(nwPATH.dirname(in_path));
+                b_editor.setFile(normalizePath(in_path));
+                */
+            }
+        }
+    });
+}
+
+function dispatchEvent(ev_name, ev_properties) {
+    var new_event = new CustomEvent(ev_name, ev_properties);
+    document.dispatchEvent(new_event);
+}
+
+function normalizePath(path) {
+    return path.replace(/(\/|\\)/g, '/');
+}
+
+function escapeHtml(text) {
+  return text
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+}
+
+
+function chooseFile(path, callback) {
+    eIPC.send('open-file-dialog');
+    eIPC.on('selected-directory', function (event, path) {
+        callback(path);
+    })
+}
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4();
+}
+
+String.prototype.hashCode = function(){
+	var hash = 0;
+	if (this.length == 0) return hash;
+	for (i = 0; i < this.length; i++) {
+		char = this.charCodeAt(i);
+		hash = ((hash<<5)-hash)+char;
+		hash = hash & hash; // Convert to 32bit integer
+	}
+	return Math.abs(hash).toString();
+}
+
+function copyObj(obj) {
+    return JSON.parse(JSON.stringify(obj));
+}
+
+function shortenPath(path, length) {
+    var path_parts = path.split(/[/]|[\\]/g);
+    if (path_parts.length > length) {
+        return nwPATH.normalize(path_parts.splice(path_parts.length - length, length).join(nwPATH.sep));
+    } else {
+        return path;
+    }
+}
+
+function fillSelect(selector, values, selected_value, capitalize=true) {
+    var html = '';
+    for (var i = 0; i < values.length; i++) {
+        var selected = '';
+        if (values[i] === selected_value) {
+            selected = ' selected ';
+        }
+        var new_val = values[i];
+        if (capitalize) {
+            var new_val = values[i].charAt(0).toUpperCase() + values[i].slice(1);
+        }
+        html += "<option value='" + values[i] + "'" + selected + ">" + new_val + "</option>";
+    }
+    $(selector).html(html);
+}
+
+Array.prototype.includesMulti = function(arr){
+    var is_there = false;
+    this.map(function(val) {
+        is_there = (arr.includes(val));
+    });
+    return is_there;
+}
+
+function obj_assign(obj, prop, value) {
+    if (typeof prop === "string")
+        prop = prop.split(".");
+
+    if (prop.length > 1) {
+        var e = prop.shift();
+        obj_assign(obj[e] =
+                 Object.prototype.toString.call(obj[e]) === "[object Object]"
+                 ? obj[e]
+                 : {},
+               prop,
+               value);
+    } else
+        obj[prop[0]] = value;
+}
+
+function parseXML(str) {
+    parser = new DOMParser();
+    xmlDoc = parser.parseFromString(str, "text/xml");
+    return xmlDoc;
+}
+
+// creates a file and it's necessary directories
+function createFile() {
+
+}
---
+++ src/main.js
@@ -0,0 +1,171 @@
+const DEV_MODE = true; // show dev tools
+
+/* electron start */
+const electron = require('electron');
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+
+const {ipcMain} = require('electron');
+const {ipcRenderer} = require('electron');
+const {globalShortcut} = require('electron');
+const dialog = require('electron').dialog;
+const {Menu} = require("electron");
+
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow () {
+
+    // get data from init.json
+    var path = require("path");
+    var fs = require("fs");
+    var initPath = path.join(app.getPath('userData'), "init.json");
+    var data;
+    try {
+        data = JSON.parse(fs.readFileSync(initPath, 'utf8'));
+    }
+    catch(e) {
+    }
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        // default window settings
+        title: data.title ? data.title : "new project",
+        center: data.x ? false : true,
+        autoHideMenuBar: true,
+        width: data.width ? data.width : 800,
+        height: data.height ? data.height : 600,
+        frame: true,
+    });
+
+    mainWindow.on("close", function() {
+        var win_bounds = mainWindow.getBounds();
+
+        var data = {
+            title: "new project",
+            x: win_bounds.x,
+            y: win_bounds.y,
+            width: win_bounds.width,
+            height: win_bounds.height
+        };
+        fs.writeFileSync(initPath, JSON.stringify(data));
+    })
+
+  global.shareVars = {args: process.argv};
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  if (DEV_MODE) {
+      mainWindow.webContents.openDevTools();
+      mainWindow.webContents.send("set-devmode-true");
+  }
+
+  mainWindow.setMenu(null);
+
+  globalShortcut.register('Control+R', () => {
+      mainWindow.webContents.send("focus-search");
+  });
+/*
+    var template = [{
+        label: "Application",
+        submenu: [
+            { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+        ]}, {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]}
+    ];
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+*/
+
+}
+
+// Emitted when the window is closed.
+app.on('closed', function () {
+  // Dereference the window object, usually you would store windows
+  // in an array if your app supports multi windows, this is the time
+  // when you should delete the corresponding element.
+
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  mainWindow.webContents.send('window-close');
+  ipcMain.on('confirm-window-close', function(event) {
+      mainWindow = null;
+  });
+  app.quit();
+});
+
+app.on('will-quit', () => {
+    // Unregister all shortcuts.
+    globalShortcut.unregisterAll();
+});
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipcMain.on('open-file-dialog', function (event) {
+  dialog.showOpenDialog({
+    properties: ['openFile']
+  }, function (files) {
+    if (files) event.sender.send('selected-directory', files);
+  });
+});
+
+ipcMain.on('show-dev-tools', function(event) {
+    mainWindow.webContents.openDevTools();
+});
+
+ipcMain.on('set-win-title', function(event, title) {
+    mainWindow.setTitle(title);
+});
+
+ipcMain.on('close', function(event) {
+    console.log('close');
+    mainWindow.close();
+});
+
+ipcMain.on('maximize', function(event) {
+    console.log('max');
+    if (mainWindow.isMaximized()) {
+        mainWindow.unmaximize();
+    } else {
+        mainWindow.maximize();
+    }
+});
+
+ipcMain.on('minimize', function(event) {
+    console.log('min');
+    if (mainWindow.isMinimized()) {
+        mainWindow.restore();
+    } else {
+        mainWindow.minimize();
+    }
+});
