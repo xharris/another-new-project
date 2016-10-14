57692b0c183c8202e39e8505a78b99f133ab16a0
Xavier Harris <xharris1@umbc.edu>
2016-10-14 17:38:43 -0400

    grid dragging works

--- src/js/modules/scene.js
+++ src/js/modules/scene.js
@@ -4,11 +4,13 @@ var grid_settings = {
 	'height': 32,
 	'width': 32
 }
+var grid_opacity = 0.1;
 var grid_tiles;
 var camera = {x: 0, y: 0};
 
 var cursors;
 var pGraphics;
+var origin_g;
 
 exports.loaded = function() {
 
@@ -25,6 +27,8 @@ exports.onDblClick = function(uuid, properties) {
 
 	sel_uuid = uuid;
 	sel_prop = properties;
+
+	//$("#main-canvas").
 
 	// loadScene(sel_prop.map);
 }
@@ -46,12 +50,6 @@ document.addEventListener("canvas.create", function() {
 document.addEventListener("canvas.update", function() {
 	var game = b_canvas.pGame;
 
-	/*
-	cursors.left
-
-	grid_tiles.tilePosition.x = 0;
-	grid_tiles.tilePosition.y = 0;
-	*/
 });
 
 document.addEventListener("canvas.render", function() {
@@ -59,44 +57,69 @@ document.addEventListener("canvas.render", function() {
 
 });
 
-var grid_pointer_start = {x:0, y:0};
+var camera_start = {x:0, y:0};
+
 function createGrid() {
 	var game = b_canvas.pGame;
 
+	// draw grid tile
 	pGraphics = game.add.graphics(0,0);
 
-	pGraphics.lineStyle(0.5, 0x000000, 0.5);
+	pGraphics.lineStyle(0.5, 0x000000, grid_opacity);
 	pGraphics.moveTo(0,0);
 	pGraphics.lineTo(grid_settings.width,0);
 	pGraphics.lineTo(grid_settings.width,grid_settings.height);
 	pGraphics.lineTo(0,grid_settings.height);
 	pGraphics.lineTo(0,0);
+
+	// draw origin lines
+	origin_g = game.add.graphics(0,0);		//origin_g.clear();
+	origin_g.lineStyle(1, 0x000000, grid_opacity);
+	origin_g.moveTo(0 + camera.x, 0);
+	origin_g.lineTo(0 + camera.x,game.height);
+	origin_g.moveTo(0,0 + camera.y);
+	origin_g.lineTo(game.width,0 + camera.y);
 	
 	grid_tiles = game.add.tileSprite(0, 0, game.width, game.height, pGraphics.generateTexture(1,0,-2));
 	grid_tiles.inputEnabled = true;
 	grid_tiles.input.enableDrag();
-	grid_tiles.events.onDragStart.add(function(sprite, pointer) {
+
+	grid_tiles.events.onDragStart.add(function(sprite, pointer, x, y) {
 		sprite.x = 0;
 		sprite.y = 0;
-		grid_pointer_start.x = pointer.x;
-		grid_pointer_start.y = pointer.y;
 	}, this);
-    grid_tiles.events.onDragUpdate.add(function(sprite, pointer) {
+
+	grid_tiles.events.onDragStop.add(function(sprite, pointer) {
+		camera_start.x = camera.x;
+		camera_start.y = camera.y;
+	}, this);
+
+    grid_tiles.events.onDragUpdate.add(function(sprite, pointer, x, y) {
 		sprite.x = 0;
 		sprite.y = 0;
-    	camera = {
-    		x: pointer.x - grid_pointer_start.x,
-    		y: pointer.y - grid_pointer_start.y
-    	}
-    	grid_tiles.tilePosition.x = camera.x;
+
+		camera.x = camera_start.x + x;
+		camera.y = camera_start.y + y;
+		
+		grid_tiles.tilePosition.x = camera.x;
 		grid_tiles.tilePosition.y = camera.y;
-    }, this);
-    grid_tiles.events.onDragEnd.add(function(sprite, pointer) {
-		sprite.x = 0;
-		sprite.y = 0;
-		grid_pointer_start.x = pointer.x;
-		grid_pointer_start.y = pointer.y;
-    }, this);
+		
+		// update origin position
+		origin_g.clear();
+		origin_g.lineStyle(1, 0x000000, grid_opacity);
+		origin_g.moveTo(0 + camera.x, 0);
+		origin_g.lineTo(0 + camera.x,game.height);
+		origin_g.moveTo(0,0 + camera.y);
+		origin_g.lineTo(game.width,0 + camera.y);
+		
+	}, this);
 
 	pGraphics.destroy();
+}
+
+function setGridSize(width, height) {
+	grid_settings.width = width;
+	grid_settings.height = height;
+	grid_tiles.remove();
+	createGrid();
 }
