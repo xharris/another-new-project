71709fc51c5ce50a958226eaac483d9fe7021853
Xavier Harris <xharris1@umbc.edu>
2017-03-22 14:30:44 -0400

    map loads collision boxes

--- src/engines/love2d/plugins/blanke/Map.lua
+++ src/engines/love2d/plugins/blanke/Map.lua
@@ -11,12 +11,17 @@ Map = Class{
 		self._images = {}
 		self._batches = {}
 
+		-- layers (tiles, images)
 		self.tilesets = {} 		-- {firstgid = tileset}
 		self.tilelayers = {}	-- {name = layer}
 		self.objectgroups = {}	-- {name = layer}
+		
+		-- things that aren't just images
 		self.entities = {}
+		self.shapes = {}		-- collision boxes
 
-		self._layers = {} 	-- for draw order
+		-- list of all layers. used for draw order.
+		self._layers = {} 	
 
 		-- load tilesets
 		for t, tileset in pairs(self.data.tilesets) do
@@ -68,8 +73,6 @@ Map = Class{
 						local frame_x = frame % columns * tileset.tilewidth
 						local frame_y = math.floor(frame / columns) * tileset.tileheight
 
-						print(d .. " -> " .. frame .. " - " .. frame_x .. ", " .. frame_y .. " (" .. tile_x .. ", " .. tile_y .. ") " .. tileset.name)
-
 						-- offset for tileset smaller than grid
 						local offx = 0--(tileset.tilewidth < self.data.tilewidth) and self.data.tilewidth - tileset.tilewidth or 0
 						local offy = (tileset.tileheight < self.data.tileheight) and self.data.tileheight - tileset.tileheight or 0
@@ -99,10 +102,65 @@ Map = Class{
 				if layer.name == "collision" then
 					for i_o, object in ipairs(layer.objects) do
 
+						if object.shape == "rectangle" then
+							self:addShape(object.name, object.shape, {object.x, object.y, object.width, object.height}, object.type, layer.offsetx, layer.offsety)
+						end
+
+						if object.shape == "ellipse" then
+							self:addShape(object.name, "circle", {object.x + (self.data.tilewidth/2), object.y + (self.data.tileheight/2), object.width/2}, object.type, layer.offsetx, layer.offsety)
+						end
+
+						if object.shape == "polygon" then
+							local points = {}
+							for i_p, point in ipairs(object.polygon) do
+								table.insert(points, point.x + object.x)
+								table.insert(points, point.y + object.y)
+							end
+							print_r(points)
+							self:addShape(object.name, "polygon", points, object.type, layer.offsetx, layer.offsety)
+						end
+
 					end
 				end
 			end
 		end
+	end,
+
+	-- add a collision shape
+	-- str shape: rectangle, polygon, circle, point
+	-- str name: reference name of shape
+	addShape = function(self, name, shape, args, tag, xoffset, yoffset)
+		local new_shape
+
+		-- args {x, y, width, height}
+		if shape == "rectangle" then
+			args[1] = args[1] + xoffset
+			args[2] = args[2] + yoffset
+			new_shape = HC.rectangle(unpack(args))
+
+		-- args {x1, y1, x2, y2, ...}
+		elseif shape == "polygon" then
+			for a = 1, #args, 2 do
+				print(args[a] .. " " .. args[a+1])
+				args[a] = args[a] + xoffset
+				args[a+1] = args[a+1] + yoffset
+			end
+			new_shape = HC.polygon(unpack(args))
+
+		-- args {x, y, radius}
+		elseif shape == "circle" then
+			args[1] = args[1] + xoffset
+			args[2] = args[2] + yoffset
+			new_shape = HC.circle(unpack(args))
+		end
+
+		new_shape.xoffset = args[1] - xoffset
+		new_shape.yoffset = args[2] - yoffset
+		new_shape.name = name
+		new_shape.tag = tag
+		table.insert(self.shapes, new_shape)
+
+		HC.register(new_shape)
 	end,
 
 	getEntity = function (self, type, name) 
@@ -123,6 +181,13 @@ Map = Class{
 
 	end,
 
+	debugCollision = function(self)
+		-- draw collision shapes
+		for s, shape in pairs(self.shapes) do
+			shape:draw("line")
+		end
+	end,
+
 	draw = function (self)
 		for i_l, _layer in ipairs(self._layers) do
 			-- TILELAYER
@@ -140,14 +205,6 @@ Map = Class{
 						love.graphics.draw(batch, 0, 0)
 						love.graphics.pop()
 					end
-				end
-			end
-
-			-- OBJECTGROUP
-			if _layer.type == "objectgroup" then
-				-- COLLISION BOXES
-				if _layer.name == "collision" then
-					-- debug drawing only (add later)
 				end
 			end
 		end
