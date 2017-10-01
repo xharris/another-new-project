17c2714fd24540c1b23efdd4955133f767eb635f
Xavier Harris <xharris1@umbc.edu>
2017-10-01 02:52:42 -0400

    tweaked result selection

--- src/widgets/searchbar.py
+++ src/widgets/searchbar.py
@@ -25,6 +25,8 @@ class Searchbar:
 			print("hi there")
 		self.addResult("run", "run demo", onselect)
 		self.addResult("exit", "leave this thang", onselect)
+		self.addResult("exit", "leave this thang", onselect)
+		self.addResult("exit", "leave this thang", onselect)
 
 	def onChange(self, ev):
 		print ev.get()
@@ -32,19 +34,34 @@ class Searchbar:
 	def addResult(self, text, tooltip, fn_onSelect):
 		new_result = Result(self, text, tooltip, fn_onSelect)
 		self.results.append(new_result)
+		return new_result
 
 class Result:
 	def __init__(self, searchbar, text, tooltip, fn_onSelect):
 		self.searchbar = searchbar
 		self.app = self.searchbar.app
 		self.text = text
+		self.selected = False
 		self.fn_onSelect = fn_onSelect
 
-		result_row = bFrame(self.app, self.searchbar.result_frame) #, bd=1, relief='solid')
+		self.result_row = bFrame(self.app, self.searchbar.result_frame, relief='solid') #, bd=1, )
 
-		result_text = bLabel(self.app, result_row, text=text, anchor='w')
-		result_text.pack(side=LEFT, fill=X, expand=True)
-		result_tooltip = bLabel(self.app, result_row, text=tooltip, fg=self.app.color('tooltip'))
-		result_tooltip.pack(anchor=E, side=RIGHT)
+		self.result_text = bLabel(self.app, self.result_row, text=text, anchor='w')
+		self.result_text.pack(side=LEFT, fill=X, expand=True)
+		self.result_tooltip = bLabel(self.app, self.result_row, text=tooltip, fg=self.app.color('tooltip'))
+		self.result_tooltip.pack(anchor=E, side=RIGHT)
 
-		result_row.pack(side=TOP, fill=X, expand=True)
+		self.result_row.pack(side=TOP, fill=X, expand=True, padx=1, pady=1)
+
+		self.result_row.bind("<Enter>", self.select)
+		self.result_row.bind("<Leave>", self.deselect)
+
+	def select(self, ev=None):
+		self.selected = True
+		self.result_row.configure(bd=1)
+		self.result_row.pack_configure(padx=0, pady=0)
+
+	def deselect(self, ev=None):
+		self.selected = False
+		self.result_row.configure(bd=0)
+		self.result_row.pack_configure(padx=1, pady=1)
