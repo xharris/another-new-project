a5106519ff559edeac68975b5ab92b57f04f042e
Xavier Harris <xharris1@umbc.edu>
2017-10-07 20:12:55 -0400

    not much

--- src/projects/myproject/scripts/state/state0.lua
+++ src/projects/myproject/scripts/state/state0.lua
@@ -1,38 +1,26 @@
 -- other functions: load(), leave()
 
+local x, y, offsetx, offsety
+
 -- Called every time when entering the state.
 function state0:enter(previous)
-    lorenz_canvas = Canvas()
-    lorenz_view = View()
-    lorenz_canvas.auto_clear = false
+    canv1 = Canvas()
+    view1 = View()
     
-    x = .01
-    y = 0
-    z = 0
-    t = 1
-    
-    a = 10
-    b = 2.66
-    c = 28
+    x = game_width/2
+    y = game_height/2
+    offsetx = 0
+    offsety = 0
 end
 
-local offset = 100
 function state0:update(dt)
-    t = .05-- t + 1
-    
-    -- lorenz
-    local dx = (a*(y - x))*t
-    local dy = (x*(b - z) - y)*t
-    local dz = (x*y - c*z)*t
-    
-    x = x + dx
-    y = y + dy
-    z = z + dz
-    
-    lorenz_view:moveToPosition(x+offset,y+offset)
-    lorenz_view.port_width = game_widh
-    lorenz_view.port_height = game_height
-    lorenz_view:zoom(10)
+    offsetx = sinusoidal(0,100)
+    offsety = sinusoidal(0,100)
+   
+    view1:moveToPosition(x+offsetx,y+offsety)
+    view1.port_width = game_widh
+    view1.port_height = game_height
+    view1:zoom(10)
 end
 
 function state0:draw()
@@ -40,13 +28,13 @@ function state0:draw()
     Draw.text(tostring(love.graphics:getWidth())..' '..tostring(love.graphics:getHeight()),200,188)
     Draw.text(tostring(x)..'\t'..tostring(y),200,200)
 
-    lorenz_canvas:draw(function()
+    canv1:draw(function()
         
-        Draw.point(x+offset,y+offset)
+        Draw.point(x+offsetx,y+offsety)
         
     end)
         
-    lorenz_view:draw(function()
-        lorenz_canvas:show()
+    view1:draw(function()
+        canv1:show()
     end)
 end	
